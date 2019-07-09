afb9257a15181224aca8b9cc68e7467b56018f68
Gerardo Ramirez <gerardogo145@gmail.com>
2019-07-09 09:27:35 -0600

    Corrigiendo reporte2

--- core/Reportes/reporte2.php
+++ core/Reportes/reporte2.php
@@ -24,33 +24,32 @@ $pdf->Cell(100,5, utf8_decode('REPORTE DE PEDIDOS POR USUARIO'), 0, 0, 'C');
 $pdf->Ln(10);
 // Seteamos la posición de la proxima celda en forma fija a 3.8 cm hacia la derecha de la pagina
 $pdf->setX(38);
-
+//Se coloca el color del fondo de las celdas en formato rgb
+$pdf->SetFillColor(148,155,255);
+//Se coloca el color del texto en formato rgb
+$pdf->SetTextColor(0,0,0);
+$pdf->setX(30);
+// Cell(ancho, Alto, texto, borde, salto de linea, alineación de texto, color)
+//convertimos el texto a utf8
+$pdf->Cell(50,10, utf8_decode('CLIENTE'),1,0,'C', true);
+$pdf->Cell(50,10, utf8_decode('USUARIO'),1,0,'C', true);
+$pdf->Cell(50,10, utf8_decode('NÚMERO DE PEDIDOS'),1,0,'C', true);
+//saldo de linea
 $pdf->Ln();
 $id = '';
 
  //Comienza a crear las filas de productos según la consulta mysql del modelo
 foreach($data as $datos){
     if(utf8_decode($datos['id_cliente']) != $id){
-        //Se coloca el color del fondo de las celdas en formato rgb
-        $pdf->SetFillColor(148,155,255);
-        //Se coloca el color del texto en formato rgb
-        $pdf->SetTextColor(0,0,0);
         $pdf->setX(30);
-        // Cell(ancho, Alto, texto, borde, salto de linea, alineación de texto, color)
-        //convertimos el texto a utf8
-        $pdf->Cell(50,10, utf8_decode('CLIENTE'),1,0,'C', true);
-        $pdf->Cell(50,10, utf8_decode('USUARIO'),1,0,'C', true);
-        $pdf->Cell(50,10, utf8_decode('NÚMERO DE PEDIDOS'),1,0,'C', true);
         $id = $datos['id_cliente'];
-        //saldo de linea
-        $pdf->Ln();
-    }
-    $pdf->setX(30);
-    
         $pdf->Cell(50,10, utf8_decode($datos['Nombre_cliente']),1,0,'C');
         $pdf->Cell(50,10, utf8_decode($datos['Usuario_cliente']),1,0,'C');
         $pdf->Cell(50,10, utf8_decode($datos['Pedidos']),1,0,'C');
-    $pdf->Ln();
+        $pdf->Ln();
+    }
+    
+        
 }
 
 $pdf->AliasNbPages();
